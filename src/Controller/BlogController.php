e83512bcec67fe4ceff2ff35408d7634bef714b8
mac <ma.castelain@gmail.com>
2018-11-22 15:38:47 +0100

    Modifying functions with dependencies'injections, bonus tutorial.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +12,10 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo) //Injection de dépendance : index contient une instance de la classe ArticleRepository (on n'oublie pas le use).
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
+//        $repo = $this->getDoctrine()->getRepository(Article::class);
+//  Symfony comprend qu'il passera à la fonction index un repository dans la variable $repo grâce à l'injection de dépendance.
 
         $articles = $repo->findAll();
 
@@ -33,10 +35,9 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id) {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-
-        $article = $repo->find($id);
+    public function show(Article $article) { // Injection de dépendance
+        //Symfony utilise le paramConverter (converti un param de la requête en objet)
+        //Symfony voit une route avec un identifiant et le besoin d'un article $article, il va donc chercher un article avec l'identifiant désiré.
 
         return $this->render('blog/show.html.twig', [
             'article' => $article
