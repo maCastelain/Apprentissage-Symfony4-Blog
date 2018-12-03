f897bbdc4ee917174c86735eef8b157a5aae6478
mac <ma.castelain@gmail.com>
2018-12-03 17:42:12 +0100

    Making the form functional concerning name, email, password witch register in databse now.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +15,17 @@ class SecurityController extends AbstractController
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration() {
+    public function registration(Request $request, ObjectManager $manager) {
         $user = new User();
 
-        $form = $this->createForm(RegistrationType::class);
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+        }
 
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView()
